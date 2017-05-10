6b8d104b5364302de5ef05cbea89308fdc0b5af1
Aaron Mondshine <aaronmondshine@Aarons-MacBook-Pro.local>
2017-05-10 13:34:52 -0700

    complete data_structures

--- lib/data_structures.rb
+++ lib/data_structures.rb
@@ -2,7 +2,7 @@
 
 # Write a method that returns the range of its argument (an array of integers).
 def range(arr)
-  # your code goes here
+  arr.sort[-1] - arr.sort[0]
 end
 
 # Write a method that returns a boolean indicating whether an array is in sorted
@@ -10,7 +10,7 @@ end
 # whether its operands are equal, e.g., 2 == 2 => true, ["cat", "dog"] ==
 # ["dog", "cat"] => false
 def in_order?(arr)
-  # your code goes here
+  arr == arr.sort
 end
 
 
@@ -18,12 +18,12 @@ end
 
 # Write a method that returns the number of vowels in its argument
 def num_vowels(str)
-  # your code goes here
+  str.downcase.count('aeiou')
 end
 
 # Write a method that returns its argument with all its vowels removed.
 def devowel(str)
-  # your code goes here
+  str.downcase.delete('aeiou')
 end
 
 
@@ -33,26 +33,28 @@ end
 # non-negative integer in descending order and as strings, e.g.,
 # descending_digits(4291) #=> ["9", "4", "2", "1"]
 def descending_digits(int)
-  # your code goes here
+  int.to_s.split("").sort.reverse
 end
 
 # Write a method that returns a boolean indicating whether a string has
 # repeating letters. Capital letters count as repeats of lowercase ones, e.g.,
 # repeating_letters?("Aa") => true
 def repeating_letters?(str)
-  # your code goes here
+  str.downcase[0] == str.downcase[1]
 end
 
 # Write a method that converts an array of ten integers into a phone number in
 # the format "(123) 456-7890".
 def to_phone_number(arr)
-  # your code goes here
+  int_to_string = arr.join.to_s
+  "(#{int_to_string[0..2]}) #{int_to_string[3..5]}-#{int_to_string[6..9]}"
 end
 
 # Write a method that returns the range of a string of comma-separated integers,
 # e.g., str_range("4,1,8") #=> 7
 def str_range(str)
-  # your code goes here
+  arr_of_ints = str.split(",").sort
+  arr_of_ints[-1].to_i - arr_of_ints[0].to_i
 end
 
 
@@ -62,6 +64,7 @@ end
 # arrays. offset=1 ensures that the value of offset is 1 if no argument is
 # provided. HINT: use the take(num) and drop(num) methods. You won't need much
 # code, but the solution is tricky!
-def my_rotate(arr, offset=1)
-  # your code goes here
+def my_rotate(arr, offset = 1)
+  offset = offset % arr.length
+  arr.drop(offset) + arr.take(offset)
 end
